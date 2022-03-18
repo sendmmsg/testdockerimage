8384dafdc74842ac937d905706640d3ea0e49b56
Pontus.Skoeldstroem <pontus.skoldstrom@acreo.se>
2022-03-18 10:18:06 +0100

    bla

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 echo $CR_PAT | docker login ghcr.io -u sendmmsg --password-stdin
-docker build -t sendmmsg/myvolume .
-id=$(docker create sendmmsg/myvolume)
-docker cp $id:/opt/deploy .
+docker build -t ghcr.io/sendmmsg/testimage:latest .
+docker push ghcr.io/sendmmsg/testimage:latest
