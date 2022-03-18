d0aff4fef46be20794275c798150923bd5b24512
Pontus.Skoeldstroem <pontus.skoldstrom@acreo.se>
2022-03-18 10:15:52 +0100

    updated and added PAT

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+echo $CR_PAT | docker login ghcr.io -u sendmmsg --password-stdin
 docker build -t sendmmsg/myvolume .
 id=$(docker create sendmmsg/myvolume)
 docker cp $id:/opt/deploy .
